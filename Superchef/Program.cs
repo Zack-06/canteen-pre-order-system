64534025f344dcce61296e1aa020387de3ad7816
wjnotl <liewwenjing34882@gmail.com>
2025-11-15 19:13:53 +0800

    test

--- Superchef/Program.cs
+++ Superchef/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStatusCodePagesWithReExecute("/Error/{0}"); // hit error controller if error occurs
 
+// // Add middlewares
+// app.UseMiddleware<ExpiryCleanupMiddleware>();
+// app.UseMiddleware<AuthSessionMiddleware>();
+
+// // Add hubs
+// app.MapHub<BookingHub>("/BookingHub");
+// app.MapHub<FnbOrderHub>("/FnbOrderHub");
+// app.MapHub<AccountHub>("/AccountHub");
+
+app.MapDefaultControllerRoute();
 app.Run();
