ff96f1d69fa81760585015aa8d3473d6d8faa3de
wjnotl <liewwenjing34882@gmail.com>
2025-11-19 02:17:35 +0800

    login ui

--- Superchef/wwwroot/js/app.js
+++ Superchef/wwwroot/js/app.js
@@ -5,3 +5,18 @@ $("header #search-button").on("click", () => {
 $("header #back-button").on("click", () => {
 	$("header").removeClass("search-active")
 })
+
+const toast_message = $(".toast-container").data("toast-message")
+if (toast_message) {
+	showToast(toast_message)
+}
+
+function showToast(message) {
+	const toast = $('<div class="toast"></div>').text(message)
+	$(".toast-container").append(toast)
+	setTimeout(() => toast.addClass("show"), 100)
+	setTimeout(() => {
+		toast.removeClass("show")
+		setTimeout(() => toast.remove(), 500)
+	}, 4000)
+}
