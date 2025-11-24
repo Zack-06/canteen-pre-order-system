d56dbeaebc4b366f7682791dbe36cbaecd1d83e0
wjnotl <liewwenjing34882@gmail.com>
2025-11-25 02:10:13 +0800

    order details

--- Superchef/Models/CardModels.cs
+++ Superchef/Models/CardModels.cs
@@ -10,8 +10,8 @@ public record ItemCard
     public decimal Price { get; set; }
     public decimal Rating { get; set; }
     public int Sold { get; set; }
-    public string DeleteLink { get; set; }
-    public string DeleteMsg { get; set; }
+    public string? DeleteLink { get; set; }
+    public string? DeleteMsg { get; set; }
 }
 
 public record StoreCard
@@ -21,8 +21,8 @@ public record StoreCard
     public string Name { get; set; }
     public decimal Rating { get; set; }
     public int ItemCount { get; set; }
-    public string DeleteLink { get; set; }
-    public string DeleteMsg { get; set; }
+    public string? DeleteLink { get; set; }
+    public string? DeleteMsg { get; set; }
 }
 
 public record CategoryCard
@@ -30,4 +30,15 @@ public record CategoryCard
     public string Link { get; set; }
     public string Image { get; set; }
     public string Name { get; set; }
+}
+
+public record OrderItemCard
+{
+    public string? Link { get; set; }
+    public string Image { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Variant { get; set; }
+    public int Quantity { get; set; }
+    public decimal Subtotal { get; set; }
 }
